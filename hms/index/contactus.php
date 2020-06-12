5e2f7f68082901c69295ac6a0f051884575400bf
EsraaHassan-software <61292920+EsraaHassan-software@users.noreply.github.com>
2020-06-12 13:41:05 +0200

    Add files via upload

---
+++ hms/index/contactus.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>HMS | Contact us</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+    <link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+
+
+</head>
+<body>
+<!--start-wrap-->
+
+<!--start-header-->
+<div class="header">
+    <div class="wrap">
+        <!--start-logo-->
+        <div class="logo">
+            <a href="index.html" style="font-size: 30px;">New Heart Hospital</a>
+        </div>
+        <!--end-logo-->
+        <!--start-top-nav-->
+        <div class="top-nav">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+
+                <li class="active"><a href="contactus.php">contact Us</a></li>
+            </ul>
+        </div>
+        <div class="clear"> </div>
+        <!--end-top-nav-->
+    </div>
+    <!--end-header-->
+</div>
+<div class="clear"> </div>
+<div class="wrap">
+    <div class="contact">
+        <div class="section group">
+            <div class="col span_1_of_3">
+
+                <div class="company_address">
+                    <h2>Hospital Address  :</h2>
+                    <p>5, mohamed mokhtar street, behind opera station, dokki</p>
+
+                    <p>Cairo,Egypt</p>
+                    <p>Phone:01011361214</p>
+
+                    <p>Email: <span>info@newheart-eg.com</span></p>
+
+                </div>
+            </div>
+            <div class="clear"> </div>
+        </div>
+<!--        <div class="clear"> </div>-->
+<!--        <div class="footer">-->
+<!--            <div class="wrap">-->
+<!--                <div class="footer-left">-->
+<!--                    <ul>-->
+<!--                        <li><a href="index.php">Home</a></li>-->
+<!---->
+<!--                        <li><a href="contactus.php">contact</a></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="clear"> </div>-->
+<!--            </div>-->
+<!--        </div>-->
+        <!--end-wrap-->
+</body>
+</html>
